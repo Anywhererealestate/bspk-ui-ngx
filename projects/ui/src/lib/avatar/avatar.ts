87259050770280648b7b684963a3e576684a91cc
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-10-31 13:39:09 -0500

    wip

--- projects/ui/src/lib/avatar/avatar.ts
+++ projects/ui/src/lib/avatar/avatar.ts
@@ -1,48 +1,53 @@
-import { Component, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostBinding, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconPerson } from '../icons/person';
 
 export type SizeVariant =
-  | 'large'
-  | 'medium'
-  | 'small'
-  | 'x-large'
-  | 'x-small'
-  | 'xx-large'
-  | 'xxx-large'
-  | 'xxxx-large'
-  | 'xxxxx-large';
+    | 'large'
+    | 'medium'
+    | 'small'
+    | 'x-large'
+    | 'x-small'
+    | 'xx-large'
+    | 'xxx-large'
+    | 'xxxx-large'
+    | 'xxxxx-large';
 
 @Component({
-  selector: 'ui-avatar',
-  standalone: true,
-  imports: [CommonModule, IconPerson ],
-  templateUrl: './avatar.html',
-  styleUrls: ['./avatar.scss', '../styles/base.scss', '../styles/colors.scss'],
+    selector: 'ui-avatar',
+    standalone: true,
+    imports: [CommonModule, IconPerson],
+    templateUrl: './avatar.html',
+    styleUrls: ['./avatar.scss', '../styles/base.scss', '../styles/colors.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class Avatar {
-  @Input() name!: string;
-  @Input() size: SizeVariant = 'small';
-  @Input() color: string = 'grey';
-  @Input() initials?: string;
-  @Input() showIcon: boolean = true;
-  @Input() image?: string;
-  @Input() hideTooltip: boolean = false;
-  @Input() disabled: boolean = false;
-  @Output() avatarClick = new EventEmitter<void>();
+    @Input() name!: string;
+    @Input() size: SizeVariant = 'small';
+    @Input() color: string = 'grey';
+    @Input() initials?: string;
+    @Input() showIcon: boolean = true;
+    @Input() image?: string;
+    @Input() hideTooltip: boolean = false;
+    @Input() disabled: boolean = false;
+    @Output() avatarClick = new EventEmitter<void>();
 
-  get computedInitials(): string | undefined {
-    if (this.initials) return this.initials.slice(0, 2).toUpperCase();
-    if (this.name) {
-      const parts = this.name.split(' ').filter(Boolean);
-      return parts.map(w => w[0]).slice(0, 2).join('').toUpperCase();
+    get computedInitials(): string | undefined {
+        if (this.initials) return this.initials.slice(0, 2).toUpperCase();
+        if (this.name) {
+            const parts = this.name.split(' ').filter(Boolean);
+            return parts
+                .map((w) => w[0])
+                .slice(0, 2)
+                .join('')
+                .toUpperCase();
+        }
+        return undefined;
     }
-    return undefined;
-  }
 
-  onClick(event: Event) {
-    if (!this.disabled) {
-      this.avatarClick.emit();
+    onClick(event: Event) {
+        if (!this.disabled) {
+            this.avatarClick.emit();
+        }
     }
-  }
 }
