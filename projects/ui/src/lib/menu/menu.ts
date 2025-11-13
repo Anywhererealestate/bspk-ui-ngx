ad31121db40c27db216572f1b09de9a9bed39da4
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-12 22:18:08 -0600

    chore(Menu): [BSPK-171] Add Menu docs

--- projects/ui/src/lib/menu/menu.ts
+++ projects/ui/src/lib/menu/menu.ts
@@ -9,11 +9,30 @@ import { CommonModule } from '@angular/common';
     styleUrl: './menu.scss',
 })
 export class Menu {
+    /** The id of the element. If not provided one will be generated. */
     @Input() id?: string;
+
+    /** Identifies the parent component. Helps with styling, debugging, and/or testing purposes. */
     @Input() owner?: string;
+
+    /**
+     * A label for the menu for screen readers.
+     *
+     * This is required if the role is set to "menu" or "listbox".
+     */
     @Input() label?: string;
+
+    /** The width of the menu. */
     @Input() width?: string;
+
+    /** The ARIA role of the menu. */
     @Input() role?: string = 'menu'; // or 'listbox', etc.
+
+    /**
+     * Inline styles to apply to the element.
+     *
+     * Allows for CSS variables to be passed in as well.
+     */
     @Input() style?: { [key: string]: any };
 
     get menuStyles() {
