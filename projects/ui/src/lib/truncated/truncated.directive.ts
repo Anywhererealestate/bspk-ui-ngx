a6eacb5fe1f60cbf0dd5e84105ca4f218a1f5d95
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-17 11:51:17 -0600

    chore(Truncated): [BSPK-208] remove commented out code

--- projects/ui/src/lib/truncated/truncated.directive.ts
+++ projects/ui/src/lib/truncated/truncated.directive.ts
@@ -6,34 +6,6 @@ import { Directive, ElementRef, Renderer2, AfterViewChecked, Input } from '@angu
         '[attr.data-truncated]': 'true',
     },
 })
-// export class TruncatedDirective implements AfterViewChecked {
-//     @Input('truncated') truncatedClass: string = 'data-truncated';
-
-//     private lastContent = '';
-//     private applied = false;
-
-//     constructor(
-//         private el: ElementRef,
-//         private renderer: Renderer2,
-//     ) {}
-
-//     ngAfterViewChecked() {
-//         const element = this.el.nativeElement as HTMLElement;
-//         const content = element.textContent?.trim() || '';
-
-//         if (content !== this.lastContent) {
-//             this.lastContent = content;
-//             const isTruncated = element.scrollWidth > element.clientWidth;
-//             if (isTruncated && !this.applied) {
-//                 this.renderer.addClass(element, this.truncatedClass);
-//                 this.applied = true;
-//             } else if (!isTruncated && this.applied) {
-//                 this.renderer.removeClass(element, this.truncatedClass);
-//                 this.applied = false;
-//             }
-//         }
-//     }
-// }
 export class TruncatedDirective implements AfterViewChecked {
     @Input('truncated') set truncatedClass(val: string) {
         this._truncatedClass = val && val.trim().length > 0 ? val : 'truncated-ellipsis';
