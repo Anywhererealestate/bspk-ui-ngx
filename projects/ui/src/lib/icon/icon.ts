c904f6f3ce797a7cdb5c9adf94dd851c2bed6a2b
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-26 12:36:32 -0600

    feature(Icon): clean up commented out code

--- projects/ui/src/lib/icon/icon.ts
+++ projects/ui/src/lib/icon/icon.ts
@@ -14,38 +14,16 @@ export class UIIcon {
     name = input.required<string>();
     @Input() width?: string;
 
-    // constructor() {
-    //     const elementRef = inject(ElementRef);
-    //     const iconRegistry = inject(ICON_REGISTRY);
-
-    //     effect(async (onCleanup) => {
-    //         let canceled = false;
-    //         onCleanup(() => {
-    //             canceled = true;
-    //         });
-    //         const res = await iconRegistry.getIcon(this.name());
-    //         if (canceled) return;
-
-    //         elementRef.nativeElement.innerHTML = res;
-    //         if (this.width) elementRef.nativeElement.children[0]?.setAttribute('width', this.width);
-    //     });
-    // }
-
     constructor() {
         const elementRef = inject(ElementRef);
-        // ICON_REGISTRY is an object, not a service
         const iconRegistry = ICON_REGISTRY;
 
-        // effect(() => {
-        //     const svg = iconRegistry[this.name()];
-        //     elementRef.nativeElement.innerHTML = svg || '';
-        //     if (this.width && elementRef.nativeElement.children[0]) {
-        //         elementRef.nativeElement.children[0].setAttribute('width', this.width);
-        //     }
-        // });
         effect(() => {
+            // option 1
             // const name = this.name() as keyof typeof ICON_REGISTRY;
             // const svg = ICON_REGISTRY[name];
+
+            // option 2
             const name = this.name();
             const svg = ICON_REGISTRY[name as keyof typeof ICON_REGISTRY] ?? '';
             elementRef.nativeElement.innerHTML = svg || '';
@@ -54,17 +32,4 @@ export class UIIcon {
             }
         });
     }
-
-    // constructor() {
-    //     const elementRef = inject(ElementRef);
-
-    //     effect(() => {
-    //         const name = this.name();
-    //         const svg = ICON_REGISTRY[name] ?? '';
-    //         elementRef.nativeElement.innerHTML = svg;
-    //         if (this.width && elementRef.nativeElement.children[0]) {
-    //             elementRef.nativeElement.children[0].setAttribute('width', this.width);
-    //         }
-    //     });
-    // }
 }
