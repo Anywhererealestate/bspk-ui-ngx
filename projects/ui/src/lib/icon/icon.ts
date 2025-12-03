8544b0a57e092a5d1d615eda96d87e9ecf20d44a
anybrian <brian.reed@anywhere.re>
2025-12-03 12:45:32 -0500

    icon cleanup

--- projects/ui/src/lib/icon/icon.ts
+++ projects/ui/src/lib/icon/icon.ts
@@ -1,30 +1,24 @@
-import { Component, effect, ViewEncapsulation, inject, Input, ViewContainerRef } from '@angular/core';
+import { Component, ViewEncapsulation, inject, Input, ViewContainerRef, AfterViewInit } from '@angular/core';
 import { BspkIcon } from '../../types/bspk-icon';
 
 @Component({
     selector: 'ui-icon',
-    imports: [],
     template: '',
     styles: 'ui-icon { display: contents; }',
     encapsulation: ViewEncapsulation.None,
 })
-export class UIIcon {
+export class UIIcon implements AfterViewInit {
     /** @see https://bspk.anywhere.re/icons */
     @Input() icon!: BspkIcon;
     @Input() width?: string;
 
-    constructor() {
-        const viewContainerRef = inject(ViewContainerRef);
+    viewContainerRef = inject(ViewContainerRef);
 
-        effect(async (onCleanup) => {
-            if (typeof this.icon === 'function') {
-                viewContainerRef.clear();
-                const ref = viewContainerRef.createComponent(this.icon);
-                if (this.width) {
-                    (ref.instance as { width?: string }).width = this.width;
-                }
-                return;
-            }
-        });
+    ngAfterViewInit() {
+        if (typeof this.icon !== 'function') return;
+
+        const icon = this.viewContainerRef.createComponent(this.icon).instance;
+
+        icon.width = this.width;
     }
 }
