73c9f37a2be271c8892549576a9b9518fbd6f244
anybrian <brian.reed@anywhere.re>
2025-12-11 13:26:52 -0500

    test bug checks

--- projects/ui/src/lib/tooltip/tooltip.directive.ts
+++ projects/ui/src/lib/tooltip/tooltip.directive.ts
@@ -32,7 +32,7 @@ export type TooltipProps = {
     disabled?: boolean;
 };
 
-@Directive({ selector: '[tooltip]' })
+@Directive({ selector: '[x-tooltip]' })
 export class UITooltipDirective implements OnDestroy, OnChanges {
     /** Accepts a string (tooltip label) or a config object { label, placement, showTail, disabled } */
     @Input('tooltip') tooltip: TooltipProps | string = '';
--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -15,8 +15,6 @@ export * from './lib/dialog';
 export * from './lib/drawer';
 export * from './lib/icon';
 export * from './lib/icons';
-export * from './lib/input';
-export * from './lib/modal';
 export * from './lib/pagination';
 export * from './lib/radio';
 export * from './lib/table';
