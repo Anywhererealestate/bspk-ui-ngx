ca0f589ff5b696de0ea0749cb39692cef81bff89
anybrian <brian.reed@anywhere.re>
2025-12-11 13:00:45 -0500

    fix: add pr-checks, lint naming conventions and pr titles

---
+++ .scripts/lint.ts
@@ -0,0 +1,107 @@
+/**
+ * Ensures all component classes and selectors follow naming conventions.
+ *
+ * Ensures that the public API file is up to date.
+ *
+ * $ npx tsx .scripts/lint.ts
+ */
+import * as fs from 'fs';
+import * as path from 'path';
+import { execSync } from 'child_process';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const libDir = path.join(__dirname, '../projects/ui/src/lib');
+
+const files = fs.readdirSync(libDir, { withFileTypes: true });
+
+const classNameComponentPattern = /^UI[A-Z][a-zA-Z0-9]+$/;
+const classNameDirectivePattern = /^UI[A-Z][a-zA-Z0-9]+Directive$/;
+const componentSelectorPattern = /^ui-[a-z0-9-]+$/;
+const directiveSelectorPattern = /^\[(?!ui)[a-z0-9-]+\]$/;
+
+files.forEach((dirent) => {
+    if (dirent.isDirectory()) {
+        if (dirent.name === 'icons') return;
+
+        const componentDir = path.join(libDir, dirent.name);
+        const componentFilePath = path.join(componentDir, dirent.name + '.ts');
+        const directiveFilePath = path.join(componentDir, dirent.name + '.directive.ts');
+
+        const filesToCheck: {
+            filePath: string;
+            type: 'component' | 'directive';
+        }[] = [];
+
+        if (fs.existsSync(componentFilePath)) filesToCheck.push({ filePath: componentFilePath, type: 'component' });
+
+        if (fs.existsSync(directiveFilePath)) filesToCheck.push({ filePath: directiveFilePath, type: 'directive' });
+
+        filesToCheck.forEach(({ filePath, type }) => {
+            let content = fs.readFileSync(filePath, 'utf-8');
+
+            const classMatch = content.match(/export class (\w+)[<|\s]/);
+            const selectorMatch = content.match(/selector:\s*'([^']+)'/);
+
+            if (classMatch) {
+                const className = classMatch[1];
+
+                if (type === 'component' && !classNameComponentPattern.test(className)) {
+                    return error(
+                        `Class name "${className}" in file "${filePath}" does not follow the convention "UI[ComponentName]".`,
+                    );
+                }
+
+                if (type === 'directive' && !classNameDirectivePattern.test(className)) {
+                    return error(
+                        `Class name "${className}" in file "${filePath}" does not follow the convention "UI[DirectiveName]Directive".`,
+                    );
+                }
+            } else {
+                return error(`No class found in file "${filePath}".`);
+            }
+
+            if (selectorMatch) {
+                const selector = selectorMatch[1];
+
+                if (type === 'directive' && !directiveSelectorPattern.test(selector)) {
+                    return error(
+                        `Selector "${selector}" in file "${filePath}" does not follow the convention [directive-name]. Should be "${dirent.name}".`,
+                    );
+                }
+
+                if (type === 'component' && !componentSelectorPattern.test(selector)) {
+                    return error(
+                        `Selector "${selector}" in file "${filePath}" does not follow the convention "ui-[component-name]".`,
+                    );
+                }
+            } else {
+                return error(`No selector found in file "${filePath}".`);
+            }
+        });
+    }
+});
+
+function error(message: string) {
+    console.error('❌ Lint Error:', message);
+    process.exit(1);
+}
+
+console.log('\x1b[32mAll components and directives follow naming conventions 🎉✅\x1b[0m\n');
+
+// ensure public api is up to date
+
+// compare existing public api content to generated content
+import { getPublicApiFileContent } from './update-public-api.js';
+
+const { publicApiPath, publicApiContent } = getPublicApiFileContent();
+
+const existingPublicApiContent = fs.readFileSync(publicApiPath, 'utf-8');
+
+if (existingPublicApiContent !== publicApiContent) {
+    console.error('❌ Lint Error: Public API is out of date. Please run:');
+    console.error('\n   npx tsx .scripts/update-public-api.ts --write\n');
+    process.exit(1);
+} else {
+    console.log('\x1b[32mPublic API is up to date 🎉✅\x1b[0m\n');
+}
