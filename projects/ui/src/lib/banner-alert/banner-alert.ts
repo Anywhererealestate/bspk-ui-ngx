f4edd3aed110118b6508a4026c5846fb11620b4e
Emile Fleming <emile.fleming@anywhere.re>
2025-12-15 14:59:32 -0500

    chore: fix tests

--- projects/ui/src/lib/banner-alert/banner-alert.ts
+++ projects/ui/src/lib/banner-alert/banner-alert.ts
@@ -1,62 +1,64 @@
 import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
-import { IconCheckCircleFill, IconClose, IconErrorFill, IconInfoFill, IconWarningFill } from '../icons';
 import { UIButton } from '../button';
+import { IconCheckCircleFill, IconClose, IconErrorFill, IconInfoFill, IconWarningFill } from '../icons';
 
 export type AlertVariant = 'error' | 'informational' | 'success' | 'warning';
-export type BannerAlertCallToActionConfig = { label: string; onClick: () => void };
+export interface BannerAlertCallToActionConfig {
+    label: string;
+    onClick: () => void;
+}
 
 @Component({
-  selector: 'ui-banner-alert',
-  imports: [UIButton, IconErrorFill, IconInfoFill, IconCheckCircleFill, IconWarningFill],
-  templateUrl: './banner-alert.html',
-  styleUrl: './banner-alert.scss',
-  standalone: true,
-  encapsulation: ViewEncapsulation.None,
+    selector: 'ui-banner-alert',
+    imports: [UIButton, IconErrorFill, IconInfoFill, IconCheckCircleFill, IconWarningFill],
+    templateUrl: './banner-alert.html',
+    styleUrl: './banner-alert.scss',
+    standalone: true,
+    encapsulation: ViewEncapsulation.None,
 })
-
 export class UIBannerAlert {
-  /**
-   * The color variant of the banner alert.
-   *
-   * @default informational
-   */
-  @Input() variant?: AlertVariant = 'informational';
-  /**
-   * Is the alert elevated. If true a drop shadow is added.
-   *
-   * @default false
-   */
-  @Input() elevated?: boolean = false;
-  /**
-   * The header of the banner alert.
-   *
-   * @required
-   */
-  @Input() header: string = '';
-  /**
-   * The body of the banner alert.
-   *
-   * @type multiline
-   * @required
-   */
-  @Input() body: string = '';
-  /**
-   * This property may be undefined or an object containing required CallToActionButton properties.
-   *
-   * @type CallToActionButton
-   */
-  @Input() callToAction?: BannerAlertCallToActionConfig;
-  /**
-   * Function to call when the banner alert is closed.
-   *
-   * @type () => void
-   * @required
-   */
-  @Output() onClose = new EventEmitter<void>();
+    /**
+     * The color variant of the banner alert.
+     *
+     * @default informational
+     */
+    @Input() variant?: AlertVariant = 'informational';
+    /**
+     * Is the alert elevated. If true a drop shadow is added.
+     *
+     * @default false
+     */
+    @Input() elevated?: boolean = false;
+    /**
+     * The header of the banner alert.
+     *
+     * @required
+     */
+    @Input() header = '';
+    /**
+     * The body of the banner alert.
+     *
+     * @type multiline
+     * @required
+     */
+    @Input() body = '';
+    /**
+     * This property may be undefined or an object containing required CallToActionButton properties.
+     *
+     * @type CallToActionButton
+     */
+    @Input() callToAction?: BannerAlertCallToActionConfig;
+    /**
+     * Function to call when the banner alert is closed.
+     *
+     * @type () => void
+     * @required
+     */
+    @Output() onClose = new EventEmitter<void>();
 
-  IconClose = IconClose;
+    IconClose = IconClose;
 
-  emitClose() {
-      this.onClose.emit();
-  }
+    emitClose() {
+        this.onClose.emit();
+    }
 }
