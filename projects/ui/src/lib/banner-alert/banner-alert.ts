cefc85a49cfeed2322eef80fbcc125a13a527bb4
Emile Fleming <emile.fleming@anywhere.re>
2025-12-16 11:57:21 -0500

    chore: remove nonworking suggestion

--- projects/ui/src/lib/banner-alert/banner-alert.ts
+++ projects/ui/src/lib/banner-alert/banner-alert.ts
@@ -15,9 +15,6 @@ export interface BannerAlertCallToActionConfig {
     styleUrl: './banner-alert.scss',
     standalone: true,
     encapsulation: ViewEncapsulation.None,
-    host: {
-    ... host attrs here
-    }
 })
 export class UIBannerAlert {
     /**
