70e675e4f287220fb44e0ad468088cf68463a9f5
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-12-17 16:12:02 -0600

    chore(Radio): example clean up

--- projects/ui/src/lib/radio/example.ts
+++ projects/ui/src/lib/radio/example.ts
@@ -9,11 +9,23 @@ import { UIRadio } from './radio';
     template: `
         <h2>Radio</h2>
 
+        <h3>Basic with id</h3>
+        <ui-radio id="test-id" name="radio-0" ariaLabel="Option 0" value="Option 0"></ui-radio>
+
         <h3>checked = true</h3>
         <ui-radio name="radio-1" ariaLabel="Option 1" value="Option 1" [checked]="true"></ui-radio>
 
         <h3>checked = false</h3>
         <ui-radio name="radio-2" ariaLabel="Option 2" value="Option 2" [checked]="false"></ui-radio>
+
+        <h3>with id</h3>
+        <ui-radio
+            id="test-id"
+            name="radio-6"
+            ariaLabel="Option 6"
+            value="Option 6"
+            [checked]="false"
+            [invalid]="true"></ui-radio>
 
         <h3>checked = true, disabled = true</h3>
         <ui-radio name="radio-3" ariaLabel="Option 3" value="Option 3" [checked]="true" [disabled]="true"></ui-radio>
@@ -24,14 +36,8 @@ import { UIRadio } from './radio';
         <h3>checked = true, invalid = true</h3>
         <ui-radio name="radio-5" ariaLabel="Option 5" value="Option 5" [checked]="true" [invalid]="true"></ui-radio>
 
-        <h3>checked = false, invalid = true, with id</h3>
-        <ui-radio
-            id="test-id"
-            name="radio-6"
-            ariaLabel="Option 6"
-            value="Option 6"
-            [checked]="false"
-            [invalid]="true"></ui-radio>
+        <h3>checked = false, invalid = true</h3>
+        <ui-radio name="radio-6" ariaLabel="Option 6" value="Option 6" [checked]="false" [invalid]="true"></ui-radio>
     `,
 })
 export class UIRadioExample {}
