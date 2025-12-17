2c0aca81a75df77eee5c6be8101221c467b698b1
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-12-17 13:02:19 -0600

    chore(RadioOption): add a11y check in testing

--- projects/ui/src/lib/radio-option/radio-option.spec.ts
+++ projects/ui/src/lib/radio-option/radio-option.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { hasNoBasicA11yIssues } from '../../testutils/hasNoBasicA11yIssues';
 import { UIRadioOption } from './radio-option';
 
 describe('UIRadioOption', () => {
@@ -18,4 +19,6 @@ describe('UIRadioOption', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should have no basic a11y issues', async () => await hasNoBasicA11yIssues(fixture));
 });
