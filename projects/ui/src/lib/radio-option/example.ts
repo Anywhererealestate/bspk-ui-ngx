71df5993149bcba566f38b57281c9268a5c8b06b
anybrian <brian.reed@anywhere.re>
2025-12-18 00:11:50 -0500

    fix: poc for extending compoennt classes

--- projects/ui/src/lib/radio-option/example.ts
+++ projects/ui/src/lib/radio-option/example.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { sendSnackbar } from '../../utils/sendSnackbar';
-import { RadioProps } from '../radio/radio';
 import { UIRadioOption } from './radio-option';
 
 @Component({
@@ -10,113 +9,95 @@ import { UIRadioOption } from './radio-option';
     imports: [CommonModule, UIRadioOption],
     template: `
         <h2>Radio Option</h2>
-        <h3>basic</h3>
-        <ui-radio-option
-            label="Minimal props"
-            description="only name, value and id set"
-            [radioInput]="radioOptionProps(1)"
-            (checkedChange)="onCheckedChange(1, $event)">
-        </ui-radio-option>
-
-        <h3>checked = true</h3>
-        <ui-radio-option
-            label="Option 2"
-            description="This is option 2"
-            [radioInput]="radioOptionProps(2)"
-            (checkedChange)="onCheckedChange(2, $event)">
-        </ui-radio-option>
-
-        <h3>checked = true, disabled = true</h3>
-        <ui-radio-option
-            label="Option 3"
-            description="This is option 3"
-            [radioInput]="radioOptionProps(3)"
-            (checkedChange)="onCheckedChange(3, $event)">
-        </ui-radio-option>
-
-        <h3>disabled = true</h3>
-        <ui-radio-option
-            label="Option 4"
-            description="This is option 4"
-            [radioInput]="radioOptionProps(4)"
-            (checkedChange)="onCheckedChange(4, $event)">
-        </ui-radio-option>
-
-        <h3>checked = true, invalid = true</h3>
-        <ui-radio-option
-            label="Option 5"
-            description="This is option 5"
-            [radioInput]="radioOptionProps(5)"
-            (checkedChange)="onCheckedChange(5, $event)">
-        </ui-radio-option>
-
-        <h3>invalid = true</h3>
-        <ui-radio-option
-            label="Option 6"
-            description="This is option 6"
-            [radioInput]="radioOptionProps(6)"
-            (checkedChange)="onCheckedChange(6, $event)">
-        </ui-radio-option>
-
-        <h3>required = true</h3>
-        <ui-radio-option
-            label="Option 7"
-            description="This is option 7"
-            [radioInput]="radioOptionProps(7)"
-            (checkedChange)="onCheckedChange(7, $event)">
-        </ui-radio-option>
-
-        <h3>required = false</h3>
-        <ui-radio-option
-            label="Option 8"
-            description="This is option 8"
-            [radioInput]="radioOptionProps(8)"
-            (checkedChange)="onCheckedChange(8, $event)">
-        </ui-radio-option>
-
-        <h3>disabled = true, invlaid = true</h3>
-        <ui-radio-option
-            label="Option 9"
-            description="This is option 9"
-            [radioInput]="radioOptionProps(9)"
-            (checkedChange)="onCheckedChange(9, $event)">
-        </ui-radio-option>
-
-        <h3>checked = true, disabled = true, invlaid = true</h3>
-        <ui-radio-option
-            label="Option 10"
-            description="This is option 10"
-            [radioInput]="radioOptionProps(10)"
-            (checkedChange)="onCheckedChange(10, $event)">
-        </ui-radio-option>
+        @for (ex of examples; track $index) {
+            <h3>{{ ex.label }}</h3>
+            <ui-radio-option
+                [label]="'This is ' + ex.label"
+                [description]="'This is description for ' + ex.label"
+                [id]="ex.id"
+                [name]="ex.name || 'example-radio-group'"
+                [value]="ex.value || ex.label"
+                [checked]="ex.checked"
+                [disabled]="ex.disabled"
+                [invalid]="ex.invalid"
+                [required]="ex.required"
+                [ariaLabel]="ex.ariaLabel"
+                (checkedChange)="onCheckedChange($index, $event)"
+                (change)="radioOptionChange($index)" />
+        }
     `,
 })
 export class UIRadioOptionExample {
     // Option-specific config
-    optionConfig: Record<number, Partial<RadioProps>> = {
-        1: { name: 'radio1', value: 'option1', id: 'example id' },
-        2: { name: 'radio2', value: 'option2', checked: true },
-        3: { name: 'radio3', value: 'option3', checked: true, disabled: true },
-        4: { name: 'radio4', value: 'option4', disabled: true },
-        5: { name: 'radio5', value: 'option5', checked: true, invalid: true },
-        6: { name: 'radio6', value: 'option6', invalid: true },
-        7: { name: 'radio7', value: 'option7', required: true },
-        8: { name: 'radio8', value: 'option8', required: false },
-        9: { name: 'radio9', value: 'option9', invalid: true, disabled: true },
-        10: { name: 'radio10', value: 'option10', invalid: true, disabled: true, checked: true },
-    };
 
-    radioOptionProps(idx: number): RadioProps {
-        return {
-            ...this.optionConfig[idx],
-            change: () => {
-                this.onCheckedChange(idx, !this.optionConfig[idx].checked);
-            },
-        } as RadioProps;
+    examples: (Partial<{
+        label: string;
+        description: string;
+        name: string;
+        value: string;
+        checked: boolean;
+        disabled: boolean;
+        invalid: boolean;
+        required: boolean;
+        ariaLabel: string;
+    }> & { id: string })[] = [
+        {
+            label: 'basic',
+        },
+        {
+            label: 'checked = true',
+            checked: true,
+        },
+        {
+            label: 'checked = true, disabled = true',
+            checked: true,
+            disabled: true,
+        },
+        {
+            label: 'disabled = true',
+            disabled: true,
+        },
+        {
+            label: 'checked = true, invalid = true',
+            checked: true,
+            invalid: true,
+        },
+        {
+            label: 'invalid = true',
+            invalid: true,
+        },
+        {
+            label: 'required = true',
+            required: true,
+        },
+        {
+            label: 'required = false',
+            required: false,
+        },
+        {
+            label: 'disabled = true, invlaid = true',
+            invalid: true,
+            disabled: true,
+        },
+        {
+            label: 'checked = true, disabled = true, invlaid = true',
+            invalid: true,
+            disabled: true,
+            checked: true,
+        },
+    ].map((config, idx) => ({
+        id: `radio-option-example-${idx}`,
+        name: `example-radio-group-${idx}`,
+        value: `option-${idx}`,
+        ...config,
+    }));
+
+    radioOptionChange(idx: number) {
+        return () => this.onCheckedChange(idx, !this.examples[idx].checked);
     }
 
     onCheckedChange(idx: number, checked: boolean) {
-        this.optionConfig[idx].checked = checked;
+        this.examples[idx].checked = checked;
         sendSnackbar(`Radio ${idx} checked: ${checked}`);
     }
 }
--- projects/ui/src/lib/radio/radio.ts
+++ projects/ui/src/lib/radio/radio.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter, ViewEncapsulation, input, booleanAttribute } from '@angular/core';
+import { generateId } from '../../utils/generateId';
 
 /**
  * A round control that allows user to choose one option from a set. This is the base element and if used directly you
@@ -12,20 +13,6 @@ import { Component, Output, EventEmitter, ViewEncapsulation, input, booleanAttri
  * @name Radio
  * @phase Utility
  */
-
-export interface RadioProps {
-    name: string;
-    value: string;
-    ariaLabel?: string;
-    checked?: boolean;
-    disabled?: boolean;
-    id?: string;
-    invalid?: boolean;
-    required?: boolean;
-    change: (event: Event) => void;
-    selected?: boolean;
-    click?: (event: Event) => void;
-}
 
 @Component({
     selector: 'ui-radio',
@@ -59,21 +46,21 @@ export class UIRadio {
      *
      * @required
      */
-    readonly change = input<((event: Event) => void) | undefined>(undefined);
+    change = input<((event: Event) => void) | undefined>(undefined);
 
     /**
      * The [name](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#name) of the control.
      *
      * @required
      */
-    readonly name = input<string | undefined>(undefined);
+    name = input<string>();
 
     /**
      * The value of the field control.
      *
      * @required
      */
-    readonly value = input<string | undefined>(undefined);
+    value = input<string | undefined>(undefined);
 
     /**
      * The aria-label for the element.
@@ -82,24 +69,24 @@ export class UIRadio {
      *
      * Ensure this is provided when using the element in isolation to maintain accessibility.
      */
-    readonly ariaLabel = input<string | undefined>(undefined);
+    ariaLabel = input<string | undefined>(undefined);
 
     /**
      * Marks the radio as checked.
      *
      * @default false
      */
-    readonly checked = input(false, { transform: booleanAttribute });
+    checked = input(false, { transform: booleanAttribute });
 
     /**
      * Determines if the element is [disabled](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled).
      *
      * @default false
      */
-    readonly disabled = input(false, { transform: booleanAttribute });
+    disabled = input(false, { transform: booleanAttribute });
 
     /** The id of the element. If not provided one will be generated. */
-    readonly id = input<string | undefined>(undefined);
+    id = input<string>(generateId());
 
     /**
      * Indicates that the element is in an invalid state and displays the error theme.
@@ -108,14 +95,14 @@ export class UIRadio {
      *
      * @default false
      */
-    readonly invalid = input(false, { transform: booleanAttribute });
+    invalid = input(false, { transform: booleanAttribute });
 
     /**
      * Determines if the element is [required](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/required).
      *
      * @default false
      */
-    readonly required = input(false, { transform: booleanAttribute });
+    required = input(false, { transform: booleanAttribute });
 
     onInputChange(event: Event) {
         const inputElement = event.target as HTMLInputElement;
