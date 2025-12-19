702c54d14105576c4c62a0f3adee8de96b1a6cd6
anybrian <brian.reed@anywhere.re>
2025-12-19 15:25:27 -0500

    fix: tooltips, live updates, a11y fix: hide not destroy when hidden (#76)
    * ensures tooltips can handle live updates,
    ensures tooltips don't leave the dom until reference element is removed
    
    * test out tooltip destroy :)
    
    * destroy, cleanup fixes

--- projects/ui/src/types/utils.ts
+++ projects/ui/src/types/utils.ts
@@ -1,5 +1,7 @@
-import { InputSignal } from '@angular/core';
+import { InputSignal, Signal } from '@angular/core';
 
-export type AsSignal<T> = { [K in keyof T]: InputSignal<T[K] | undefined> };
+export type AsInputSignal<T> = { [K in keyof T]: InputSignal<T[K] | undefined> };
+
+export type AsSignal<T> = { [K in keyof T]: Signal<T[K] | undefined> };
 
 export type AlertVariant = 'error' | 'informational' | 'success' | 'warning';
