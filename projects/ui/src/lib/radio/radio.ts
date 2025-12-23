67b503a29d442bf7184c939f56a283b31c68bf05
anybrian <brian.reed@anywhere.re>
2025-12-23 12:49:22 -0500

    fix: id cleanup (#82)

--- projects/ui/src/lib/radio/radio.ts
+++ projects/ui/src/lib/radio/radio.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter, ViewEncapsulation, input, booleanAttribute } from '@angular/core';
-import { randomString } from '../../utils/randomString';
+import { uniqueId } from '../../utils/random';
 
 /**
  * A round control that allows user to choose one option from a set. This is the base element and if used directly you
@@ -86,7 +86,7 @@ export class UIRadio {
     readonly disabled = input(false, { transform: booleanAttribute });
 
     /** The id of the element. If not provided one will be generated. */
-    readonly id = input<string>(randomString(8));
+    readonly id = input<string>(uniqueId('radio'));
 
     /**
      * Indicates that the element is in an invalid state and displays the error theme.
--- projects/ui/src/utils/index.ts
+++ projects/ui/src/utils/index.ts
@@ -2,4 +2,4 @@ export * from './util';
 export * from './providers';
 export * from './input-control-value-accessor';
 export * from './error-formatter';
-export * from './randomString';
+export * from './random';
