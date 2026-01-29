06e96d55866d47e27e891588ed3df2b2755dedf5
anybrian <brian.reed@anywhere.re>
2026-01-29 15:34:16 -0500

    mo better example

--- projects/ui/src/lib/accordion/example.ts
+++ projects/ui/src/lib/accordion/example.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { UIAvatar } from '../..';
+import { UIAvatar, UITag } from '../..';
 import { UIExample } from '../../demo';
 import { IconCat } from '../icons/cat';
 import { UIAccordion, UIAccordionSection } from './';
@@ -8,18 +8,18 @@ import { UIAccordion, UIAccordionSection } from './';
 @Component({
     selector: 'ui-accordion-example',
     standalone: true,
-    imports: [CommonModule, UIAccordion, UIExample, UIAccordionSection, IconCat, UIAvatar],
+    imports: [CommonModule, UIAccordion, UIExample, UIAccordionSection, IconCat, UIAvatar, UITag],
     template: `
         <h4>Default (multiple open)</h4>
         <ui-example>
             <ui-accordion [singleOpen]="false">
-                <ui-accordion-section title="Felines" subtitle="With Leading">
+                <ui-accordion-section title="Felines" subtitle="With Leading Icon">
                     <span data-leading><icon-cat width="24" /></span>
                     <div>Content about cats and other felines.</div>
                 </ui-accordion-section>
 
-                <ui-accordion-section title="Dogs" subtitle="With Trailing">
-                    <span data-trailing>Trailing</span>
+                <ui-accordion-section title="Dogs" subtitle="With Trailing Tag">
+                    <span data-trailing><ui-tag label="Dogs" color="orange" /></span>
                     <p>Dog Children</p>
                 </ui-accordion-section>
 
@@ -32,17 +32,23 @@ import { UIAccordion, UIAccordionSection } from './';
         <h4>Single Open</h4>
         <ui-example>
             <ui-accordion [singleOpen]="true">
-                <ui-accordion-section title="Alpha">
+                <ui-accordion-section title="Alpha" subtitle="With Leading Avatar">
                     <span data-leading> <ui-avatar name="Alice Anderson" size="small" /></span>
                     Alpha Children</ui-accordion-section
                 >
-                <ui-accordion-section title="Beta">
+                <ui-accordion-section title="Bravo" subtitle="With Leading Avatar">
                     <span data-leading> <ui-avatar name="Bob Brown" size="small" /></span>
-                    Beta Children</ui-accordion-section
+                    Bravo Children</ui-accordion-section
                 >
-                <ui-accordion-section title="Gamma">
+                <ui-accordion-section title="Charlie" subtitle="With Leading Avatar">
                     <span data-leading> <ui-avatar name="Charlie Clark" size="small" /></span>
-                    Gamma Children</ui-accordion-section
+                    Charlie Children</ui-accordion-section
+                >
+                <ui-accordion-section title="Delta" subtitle="With Leading string" leading="DD">
+                    Delta Children</ui-accordion-section
+                >
+                <ui-accordion-section title="Echo" subtitle="With Trailing string" trailing="echo">
+                    Echo Children</ui-accordion-section
                 >
             </ui-accordion>
         </ui-example>
