c75ded9bbe44e90e3e2ec5861b0f846903a76da0
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2026-03-10 12:04:33 -0500

    chore(Slider): update and add examples using generate-examples script (#198)

--- projects/ui/src/lib/slider/example.ts
+++ projects/ui/src/lib/slider/example.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
-import { UISlider } from './slider';
+import { sendSnackbar } from '../../utils/send-snackbar';
+import { UISlider } from '../slider';
 
 @Component({
     selector: 'ui-slider-example',
@@ -7,13 +8,9 @@ import { UISlider } from './slider';
     imports: [UISlider],
     template: `
         <h4>Default</h4>
-        <ui-slider
-            [value]="values()['default']"
-            (valueChange)="update('default', $event)"
-            name="default-slider"
-            label="Default Slider"
-            [min]="0"
-            [max]="100" />
+        <ui-slider label="example-label" [max]="50" [min]="5" name="example-name" (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
 
         <h4>Range</h4>
         <ui-slider
@@ -24,44 +21,134 @@ import { UISlider } from './slider';
             [min]="0"
             [max]="100" />
 
-        <h4>Disabled</h4>
+        <h4>disabled: true</h4>
         <ui-slider
-            name="disabled-slider"
-            label="Disabled Slider"
-            [min]="0"
-            [max]="100"
-            [value]="50"
-            [disabled]="true" />
+            [disabled]="true"
+            label="example-label"
+            [max]="120"
+            [min]="20"
+            name="example-name"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
 
-        <h4>Read Only</h4>
+        <h4>disabled: false</h4>
         <ui-slider
-            name="readonly-slider"
-            label="Read Only Slider"
-            [min]="0"
-            [max]="100"
-            [value]="30"
-            [readOnly]="true" />
+            [disabled]="false"
+            label="example-label"
+            [max]="20"
+            [min]="2"
+            name="example-name"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
 
-        <h4>Custom Step</h4>
+        <h4>formatNumber Custom Formatting</h4>
         <ui-slider
-            [value]="values()['step']"
-            (valueChange)="update('step', $event)"
-            name="step-slider"
-            label="Step of 5"
+            [formatNumber]="formatExample"
+            label="Currency Format"
+            [max]="1000"
             [min]="0"
-            [max]="100"
-            [step]="5" />
-
-        <h4>Custom Formatting</h4>
-        <ui-slider
+            name="formatted-slider"
             [value]="values()['formatted']"
             (valueChange)="update('formatted', $event)"
-            name="formatted-slider"
-            label="Currency Format"
-            [min]="0"
-            [max]="1000"
-            [step]="50"
-            [formatNumber]="formatExample" />
+            [step]="50" />
+
+        <h4>label: example-label</h4>
+        <ui-slider label="example-label" [max]="33" [min]="3" name="example-name" (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>marks: true</h4>
+        <ui-slider
+            label="example-label"
+            [marks]="true"
+            [max]="44"
+            [min]="4"
+            name="example-name"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>marks: false</h4>
+        <ui-slider
+            label="example-label"
+            [marks]="false"
+            [max]="72"
+            [min]="6"
+            name="example-name"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>max: 88</h4>
+        <ui-slider label="example-label" [max]="88" [min]="8" name="example-name" (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>min: 12</h4>
+        <ui-slider
+            label="example-label"
+            [max]="224"
+            [min]="12"
+            name="example-name"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>name: example-name</h4>
+        <ui-slider label="example-label" [max]="21" [min]="3" name="example-name" (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>readOnly: true</h4>
+        <ui-slider
+            label="example-label"
+            [max]="99"
+            [min]="9"
+            name="example-name"
+            [readOnly]="true"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>readOnly: false</h4>
+        <ui-slider
+            label="example-label"
+            [max]="100"
+            [min]="10"
+            name="example-name"
+            [readOnly]="false"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>step: 12</h4>
+        <ui-slider
+            label="example-label"
+            [max]="120"
+            [min]="12"
+            name="example-name"
+            [step]="12"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>value: 10</h4>
+        <ui-slider
+            label="example-label"
+            [max]="45"
+            [min]="5"
+            name="example-name"
+            [value]="10"
+            (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
+
+        <h4>value: unknown</h4>
+        <ui-slider label="example-label" [max]="40" [min]="2" name="example-name" (valueChange)="onValueChange($event)">
+            Example Content
+        </ui-slider>
     `,
 })
 export class UISliderExample {
@@ -76,6 +163,10 @@ export class UISliderExample {
         step: 20,
         formatted: 250,
     });
+
+    onValueChange(event: any) {
+        sendSnackbar(`valueChange event: ${JSON.stringify(event)}`);
+    }
 
     update(key: keyof ReturnType<typeof this.values>, value: number | [number, number]) {
         this.values.update((current) => ({
